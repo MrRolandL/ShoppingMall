b947b622247d3d2c0e18e179f1c848237d06d045
Roland <roland.rr.lehmann@gmail.com>
2022-08-07 14:21:30 +0300

    kiosk created

--- shoppingMallAtm/Program.cs
+++ shoppingMallAtm/Program.cs
@@ -10,7 +10,6 @@
 //}
 //else
 //    Console.WriteLine("Something went wrong!");
-
 static void Atm()
 {
     static int status()
@@ -75,3 +74,135 @@ static void Atm()
     }
 }
 Atm();
+
+static int SelectOption()
+{
+    Console.WriteLine("Please select what you want do do?\n1: Buy products (type 1)\n2: Buy a lottery(type 2)\n3: Buy products and a lottery(type 3)\n4: Quit program(type 4)");
+    int customerOption = Int32.Parse(Console.ReadLine());
+    
+    if (customerOption == 1) 
+    {
+        Console.WriteLine("You chose to buy products");
+        return customerOption;
+
+    }
+    else if (customerOption == 2) 
+    {
+        Console.WriteLine("You chose to buy a lottery ticket");
+        return customerOption;
+
+    }
+    else if (customerOption == 3) 
+    { 
+        Console.WriteLine("You chose to buy products and a lottery ticket");
+        return customerOption;
+    }
+     
+     
+    Console.WriteLine("Invalid input! Program closes!");
+    return 0;
+    
+}
+SelectOption();
+
+
+static int BuyAProduct()
+{
+    //Console.WriteLine("How many bananas would you like?");
+    string[,] productList = {
+        {"1:Banana", "Banana", "1.24"},
+        {"2:Apple", "Apple", "0.57" },
+        {"3:Cigarettes", "Cigarettes", "10.8"},
+        {"4:Milk", "Milk", "2.32"},
+        {"5:Ryebread", "Ryebread", "1.45"},
+        {"6:Meat", "Meat", "5.76"},
+        {"7:Beer", "Beer", "4.95"},
+        {"8:Butter", "Butter", "2.75"},
+        {"9:Outmeal", "Outmeal", "0.95" },
+        {"10:Cheese", "Cheese", "8.88"} };
+
+    Console.WriteLine("Choose what to buy from list:");
+    for (int i = 0; i < productList.GetLength(0); i++) 
+    { 
+        Console.WriteLine(productList[i, 0]);
+    }
+    int choice = Int32.Parse(Console.ReadLine()) -1;
+    string product = productList[choice, 1 ];
+    string price = productList[choice, 2];
+    Console.WriteLine(product);
+    Console.WriteLine(price);
+    return 0;
+}
+BuyAProduct();
+
+//static void Randomstuff()
+//{
+//    int chart = Int32.Parse(Console.ReadLine());
+//    if (chart <= 0)
+//    {
+//        return 0;
+//    }
+//    else if (chart == 1)
+//    {
+//        return chart;
+//    }
+
+//    else if (chart == 2)
+//    {
+//        return chart;
+//    }
+//    else if (chart == 3)
+//    {
+//        return chart;
+//    }
+//    else if (chart == 4)
+//    {
+//        return chart;
+//    }
+//    else
+//    {
+//        Console.WriteLine("We only have 4 bananas left, do you want to have them? (yes/no)");
+//        string want4BananasOrWhat = Console.ReadLine().ToLower();
+//        if (want4BananasOrWhat == "yes")
+//        {
+//            return 4;
+//        }
+//        else if (want4BananasOrWhat == "no")
+//        {
+//            Console.WriteLine("Do you wish to have less than 4 bananas?");
+//        }
+//        string askForLessBanana = Console.ReadLine();
+//        if (askForLessBanana == "yes")
+//        {
+//            Console.WriteLine("How many do you wish to have?");
+
+//            int newBananaOrder = Int32.Parse(Console.ReadLine());
+//            if (newBananaOrder == 3)
+//            {
+//                return 3;
+//            }
+//            else if (newBananaOrder == 2)
+//            {
+//                return 2;
+//            }
+//            else if (newBananaOrder == 1)
+//            {
+//                return 1;
+//            }
+//            else
+//            {
+//                return 0;
+//            }
+//        }
+//        else if (askForLessBanana == "no")
+//        {
+//            return 4;
+//        }
+//        else
+//        {
+//            return 0;
+//        }
+//    }
+
+//}
+//Randomstuff();
